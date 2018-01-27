4cac31ae488fcc38f46b7efe83395b171f3dce63
ehsan1988 <birnaghdejavan@gmail.com>
2018-01-27 12:23:20 +0330

    commit 7
    add dotenv crossenv
    change webpack
    set firebase in heroku

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,11 +1,11 @@
 import * as firebase from 'firebase';
 const config = {
-    apiKey: "AIzaSyCHD40i-g1l1pcBAvOYw_HFFUMfGprwPwI",
-    authDomain: "expense-app-46d95.firebaseapp.com",
-    databaseURL: "https://expense-app-46d95.firebaseio.com",
-    projectId: "expense-app-46d95",
-    storageBucket: "expense-app-46d95.appspot.com",
-    messagingSenderId: "868110838179"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 firebase.initializeApp(config);
 
